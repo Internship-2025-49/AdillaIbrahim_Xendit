a7737e9b0a415f2ec33cd5aae56e834e50f5dcec
Adilla <adilla@gmail.com>
2025-03-14 15:08:51 +0700

    feat: add payment request retrieval, capture, authorization, and simulation endpoints #1

--- xendit/src/controller/paymentRequestController.ts
+++ xendit/src/controller/paymentRequestController.ts
@@ -22,3 +22,91 @@ export async function createPaymentRequest(c: Context) {
     console.error("Error creating payment request:", error);
   }
 }
+
+export async function getPaymentRequestByID(c: Context) {
+  try {
+    const id = c.req.param("id");
+    const paymentRequest =
+      await xenditPaymentRequestClient.getPaymentRequestByID({
+        paymentRequestId: id,
+      });
+    return c.json(paymentRequest, 200);
+  } catch (error) {
+    console.error("Error getting payment request by id:", error);
+  }
+}
+
+export async function getPaymentRequestCaptures(c: Context) {
+  try {
+    const id = c.req.param("id");
+    const paymentRequest =
+      await xenditPaymentRequestClient.getPaymentRequestCaptures({
+        paymentRequestId: id,
+      });
+    return c.json(paymentRequest, 200);
+  } catch (error) {
+    console.error("Error getting payment request captures:", error);
+  }
+}
+
+export async function getAllPaymentRequest(c: Context) {
+  try {
+    const paymentRequest =
+      await xenditPaymentRequestClient.getAllPaymentRequests();
+    return c.json(paymentRequest, 200);
+  } catch (error) {
+    console.error("Error getting payment requests:", error);
+  }
+}
+
+export async function capturePaymentRequest(c: Context) {
+  try {
+    const id = c.req.param("id");
+    const paymentRequest =
+      await xenditPaymentRequestClient.capturePaymentRequest({
+        paymentRequestId: id,
+      });
+    return c.json(paymentRequest, 200);
+  } catch (error) {
+    console.error("Error capturing payment request:", error);
+  }
+}
+
+export async function authorizePaymentRequest(c: Context) {
+  try {
+    const id = c.req.param("id");
+    const paymentRequest =
+      await xenditPaymentRequestClient.authorizePaymentRequest({
+        paymentRequestId: id,
+      });
+    return c.json(paymentRequest, 200);
+  } catch (error) {
+    console.error("Error authorizing payment request:", error);
+  }
+}
+
+export async function resendPaymentRequestAuth(c: Context) {
+  try {
+    const id = c.req.param("id");
+    const paymentRequest =
+      await xenditPaymentRequestClient.resendPaymentRequestAuth({
+        paymentRequestId: id,
+      });
+    return c.json(paymentRequest, 200);
+  } catch (error) {
+    console.error("Error resending payment request auth:", error);
+  }
+}
+
+export async function simulatePaymentRequestPayment(c: Context) {
+  try {
+    const id = c.req.param("id");
+    const paymentRequest =
+      await xenditPaymentRequestClient.simulatePaymentRequestPayment({
+        paymentRequestId: id,
+      });
+    return c.json(paymentRequest, 200);
+  } catch (error) {
+    console.error("Error simulating payment request payment:", error);
+  }
+}
--- xendit/src/routes/paymentRequest.ts
+++ xendit/src/routes/paymentRequest.ts
@@ -5,4 +5,20 @@ const app = new Hono();
 
 app.post("/", (c) => PaymentRequest.createPaymentRequest(c));
 
+app.get("/:id", (c) => PaymentRequest.getPaymentRequestByID(c));
+
+app.get("/:id/captures", (c) => PaymentRequest.getPaymentRequestCaptures(c));
+
+app.get("/", (c) => PaymentRequest.getAllPaymentRequest(c));
+
+app.post("/:id/captures", (c) => PaymentRequest.capturePaymentRequest(c));
+
+app.post("/:id/auth", (c) => PaymentRequest.authorizePaymentRequest(c));
+
+app.post("/:id/auth/resend", (c) => PaymentRequest.resendPaymentRequestAuth(c));
+
+app.post("/:id/payments/simulate", (c) =>
+  PaymentRequest.simulatePaymentRequestPayment(c)
+);
+
 export default app;
